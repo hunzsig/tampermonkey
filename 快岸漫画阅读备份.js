112e2b938edcbf2ecdc345ed83c26378f92106ab
hunzsig <mzyhaohaoren@qq.com>
2022-05-26 20:01:48 +0800

    up

---
+++ 快岸漫画阅读备份.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         快岸漫画阅读备份
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  边看漫画边备份快岸图片
+// @author       hunzsig
+// @match        *://www.kanbook.net/*
+// @icon         http://www.kanbook.net/static/comic_kanbook/images/favicon.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  document.body.style.background = "#272727";
+
+  function down() {
+    let s = document.getElementById("img_c_0").src;
+    let ss = s.split(".")
+    let suffix = ss[ss.length - 1]
+    if (suffix === "jpg" || suffix === "png" || suffix === "webp") {
+      let u = document.URL.split("/");
+      let n = "";
+      if (u.length >= 6) {
+        n = u[3] + "_" + u[4] + "_" + u[5] + "_" + (u[6] || "1");
+      }
+      fetch(s).then(res => res.blob().then(blob => {
+        let a = document.createElement('a');
+        let p = window.URL.createObjectURL(blob);
+        console.log(p);
+        a.href = p;
+        a.download = n + "." + suffix;
+        a.click();
+        window.URL.revokeObjectURL(p);
+      }))
+      return true
+    }
+    return false
+  }
+
+  let t = window.setInterval(function () {
+    if (down()) {
+      window.clearInterval(t)
+    }
+  }, 100)
+
+})();
